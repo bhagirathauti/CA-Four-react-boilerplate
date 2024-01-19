1cce1e5a5fd9d4aea303e8e4bbb2e7e9e4f769a6
bhagirathauti <bhagirath961778@gmail.com>
2024-01-19 17:47:55 +0530

    toggle still left

--- src/components/QuestionBox.jsx
+++ src/components/QuestionBox.jsx
@@ -1,43 +1,75 @@
 import React, { useState,useEffect} from 'react'
 import questions from '../questions'
 import "./QuestionBox.css"
+import Result from './Result'
+import { MdLightMode } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
 
 export default function QuestionBox() {
   const [currIndex,setCurrIndex] = useState(0)
   const [currScore,setCurrScore] = useState(0)
+  const [highLighted, setHighlighted] = useState(false);
+  const [currentMode,setCurrentMode] = useState(false);
   const currQuestion = questions[currIndex];
-  useEffect(() => {
-    localStorage.setItem('quizScore', JSON.stringify(currScore));
-  }, [currScore]);
-  const buttonClick =() =>{
-    if (currQuestion.options.some(option => option.isCorrect == true)) {
-      setCurrScore(lastScore => lastScore + 10);
-      setCurrIndex(lastIndex => lastIndex+1);
+  const buttonClick = (selectedOption) => {
+    if (selectedOption.isCorrect) {
+      setCurrScore((lastScore) => lastScore + 20);
     }
-    else{
-      setCurrIndex(lastIndex => lastIndex+1);
-    }
+  
+    setCurrIndex((lastIndex) => lastIndex + 1);
+    setHighlighted(false);
+  };
+  const highlightText = () => {
+    setHighlighted(true);
+  };
+  const removeHighlight = () => {
+    setHighlighted(false);
+  };
+  const handleRestart = () => {
+    setCurrIndex(0);
+    setCurrScore(0);
+  };
+  const darkMode = () =>{
+    setCurrentMode(true)
+  }
+  const lightMode = () => {
+    setCurrentMode(false)
   }
   return(
+    <div className={`quiz-app ${currentMode ? 'darken' : ''}`}>
     <div>
-      <div className="navbar">
+      <div className={`navbar ${currentMode ? 'darken' : ''}`}>
         <h1>Kalvium</h1>
-        <button>LIGHT</button>
+        <div className="lightNdark">
+        <button className='light' onClick={lightMode}><MdLightMode /></button>
+        <button className='dark' onClick={darkMode}> <MdDarkMode/> </button>
+        </div>
     </div>
       {
         currQuestion ? (
-          <div className='QuestionsBox'>
+          <div className={`QuestionsBox ${currentMode ? 'darken' : ''}`}>
           <h6>Question {currIndex+1} of 5</h6>
-          <h2>{currQuestion.text}</h2>
-          <div className="options">
-            <button onClick={buttonClick}>A. {currQuestion.options[0].text}</button>
-            <button onClick={buttonClick}>B. {currQuestion.options[1].text}</button>
-            <button onClick={buttonClick}>C. {currQuestion.options[2].text}</button>
-            <button onClick={buttonClick}>D. {currQuestion.options[3].text}</button>
+          <h2 className={`question ${highLighted ? 'highlighted' : ''}`}>{currQuestion.text}</h2>
+          <div className={`options ${currentMode ? 'darken' : ''}`}>
+          {currQuestion.options.map((option, index) => (
+           <button className={`optBtn ${currentMode ? 'darken' : ''}`} key={option.id} onClick={() => buttonClick(option)}>
+          {['A', 'B', 'C', 'D'][index]}. {option.text}
+           </button>
+          ))}
+          </div>
+          <div className="highlightBtns">
+            <button onClick={highlightText}>HIGHLIGHT</button>
+            <button onClick={removeHighlight}>REMOVE HIGHLIGHT</button>
           </div>
           </div>
-        ):(<p>No More Questions</p>)
+        ):(
+          <>
+          <div className={`resultPage ${currentMode ? 'darken' : ''}`}>
+        <p><Result score={currScore} onRestart={handleRestart}/></p>
+        </div>
+        </>)
       }
+    </div>
     </div>
   )
 }
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import QuestionBox from './QuestionBox'
+import "./Result.css"
 
-export default function Result() {
+export default function Result({score,onRestart}) {
+  const restartAction = () => {
+    if (onRestart) {
+      onRestart();
+    }
+  }
   return (
     <div>
-      
+      <div className="resultBox">
+      <p>{score/20} out of 5 are correct({score}%)</p>
+      <button onClick={restartAction}>RESTART</button>
+      </div>
     </div>
   )
 }
